0e5e2d89d30921f93513a86fbe51c45413880140
Fili96 <fili960813@gmail.com>
2018-09-26 23:26:35 -0500

    programas de apoyo

---
+++ src/mx/uam/compiladores/hola-mundo/diccionarios.py
@@ -0,0 +1,52 @@
+# Diccionarios
+fili = {
+        "id": 1,
+        "Nombre": "Angel",
+        "Apellido": "Rebollo"
+    }
+
+print fili
+
+# Base de Datos con diccionarios
+DB = [
+        {
+            "id": 1,
+            "Nombre": "Angel",
+            "Apat": "Rebollo",
+            "Amat": "Lopez",
+            "Edad": 22,
+        },
+        {
+            "id": 2,
+            "Nombre": "Paco",
+            "Apat": "Carrillo",
+            "Amat": "Pachueco",
+            "Edad": 22,
+        },
+        {
+            "id": 3,
+            "Nombre": "Ricardo",
+            "Apat": "Chula",
+            "Amat": "Raya",
+            "Edad": 22,
+        },
+        {
+            "id": 4,
+            "Nombre": "Mary",
+            "Apat": "Contreras",
+            "Amat": "Vidal",
+            "Edad": 22,
+        }
+    ]
+
+print
+print(DB[0])
+
+print
+print(DB[1])
+
+print
+print(DB[2])
+
+print
+print(DB[3])
