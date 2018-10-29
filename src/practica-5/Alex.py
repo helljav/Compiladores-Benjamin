af6c7fd08741e75a4e83679249d920b5a8912255
javierhell <miromancequimico_004@hotmail.com>
2018-10-29 12:41:07 -0600

    practica 5 terminada

--- src/practica-5/Alex.py
+++ src/practica-5/Alex.py
@@ -69,168 +69,20 @@ class Alex(object):
             return "\0"
     
     ##
-    # Metodo que implementa el diagrama de transiciones
-    # Para el analizador Lexicografico
-    #
-    #   @Param uami: 
-    #       Un objeto de la clase Uami
-    ##
-    def alexico(self, uami):
-        
-        pr = Palabras_Reservadas()
-        uami.lineas = self.contador
-        lexema = self.leerCaracter()
-
-        if lexema is '=':
-
-            lexema = lexema + self.leerCaracter()
-
-            if lexema[ 1 ] != '=':
-
-                self.desleer()
-
-                return {
-                            "token": pr.asignacion,
-                            "lexema": lexema
-                        }
-            else:
-                return {
-                            "token": pr.error,
-                            "lexema": lexema
-                        }
-
-        elif lexema is '+':
-
-            lexema = lexema + self.leerCaracter()
-
-            if lexema[ 1 ] != '+':
-
-                self.desleer()
-
-                return {
-                            "token": pr.suma,
-                            "lexema": lexema[ 0 ]
-                        }
-            else:
-                return {
-                            "token": pr.error,
-                            "lexema": lexema
-                        }
-
-        elif lexema is '-':
-
-            lexema = lexema + self.leerCaracter()
-
-            if lexema[ 1 ] != '-':
-
-                self.desleer()
-
-                return {
-                            "token": pr.resta,
-                            "lexema": lexema[ 0 ]
-                        }
-            else:
-                return {
-                            "token": pr.error,
-                            "lexema": lexema
-                        }
-
-        # search regresa el numero de ocurrencias en la Exp. Regular
-        elif self.esDigito( lexema ): 
-
-            if lexema is "0":
-
-                lexema = lexema + self.leerCaracter()
-
-                # Cualquier cosa
-                if self.esDigito( lexema[1] ) == False:
-
-                    self.desleer()
-
-                    return {
-                            "token": pr.entero,
-                            "lexema": lexema[ 0 ]
-                        }
-
-                # Otro Numero despues del 0
-                else:
-
-                    digito = self.leerCaracter()
-
-                    while self.esDigito( digito ):
-                        lexema = lexema + digito
-                        digito = self.leerCaracter()
-                    
-                    return {
-                            "token": pr.error,
-                            "lexema": lexema
-                        }
-            else:
-
-                digito = lexema
-                lexema = ""
-
-                while self.esDigito( digito ):
-                    lexema = lexema + digito
-                    digito = self.leerCaracter()
-                    
-                self.desleer()
-
-                return {
-                            "token": pr.entero,
-                            "lexema": lexema
-                        }
-        
-        elif lexema is "\0":
-
-            return {
-                        "token": pr.hecho,
-                        "lexema": lexema
-                    }
-
-        elif lexema is " ":
-            
-            return{ 
-                "token": "vacio",
-                "lexema": lexema
-            }
-        elif self.esLetra(lexema):
-            caracter = lexema
-            lexema = ""
-
-            while self.esLetra( caracter ):
-                lexema = lexema + caracter
-                caracter = self.leerCaracter()
-                
-            self.desleer()
-            if lexema == "print":
-                return {
-                        "token": pr.imprimir,
-                        "lexema": lexema
-                    }
-            else:
-                return {
-                        "token": pr.letras,
-                        "lexema": lexema
-                    }
-
-            
-        else:
-            
-            return {
-                        "token": pr.error,
-                        "lexema": lexema
-                    }
-        
-    ##
     # Metodo que retrocede una posicion en la linea
     # de texto cargada en el buffer
     # ( Simula el signo de pesos )
     ##
     def desleer(self):
+        
         self.buffer["pos_leida"] = self.buffer["pos_leida"] - 1
 
 
+    ##
+    # Metodo para reconocer si un caracter es un digito
+    #   @param: caracter a evaluar
+    #   @Return: True si es digito, False si no
+    ##
     def esDigito( self, caracter ):
 
         digitos = [
@@ -253,9 +105,11 @@ class Alex(object):
         
         return False
 
-    #
-    #Metodo para reconocer si un caracter es una letra 
-    #
+    ##
+    # Metodo para reconocer si un caracter es una letra permitida
+    #   @param: caracter a evaluar
+    #   @Return: True si es una letra, False si no
+    ##
     def esLetra( self, caracter ):
 
         letras = [
@@ -292,5 +146,157 @@ class Alex(object):
                 return True
         
         return False
-
+    
+    ##
+    # Metodo que implementa el diagrama de transiciones
+    # Para el analizador Lexicografico
+    #
+    #   @Param uami: 
+    #       Un objeto de la clase Uami
+    ##
+    def alexico(self, uami):
         
+        pr = Palabras_Reservadas()
+        uami.lineas = self.contador
+        lexema = self.leerCaracter()
+
+        # Asignacion
+        if lexema is '=':
+
+            lexema = lexema + self.leerCaracter()
+
+            self.desleer()
+
+            return {
+                        "token": pr.asignacion,
+                        "lexema": lexema[0]
+                    }
+
+        # Suma
+        elif lexema is '+':
+
+            lexema = lexema + self.leerCaracter()
+
+            self.desleer()
+
+            return {
+                        "token": pr.suma,
+                        "lexema": lexema[ 0 ]
+                    }
+            
+        # Resta
+        elif lexema is '-':
+
+            lexema = lexema + self.leerCaracter()
+
+            self.desleer()
+
+            return {
+                        "token": pr.resta,
+                        "lexema": lexema[ 0 ]
+                    }
+
+        # Entero
+        elif self.esDigito( lexema ): 
+
+            # Empieza en cero
+            if lexema is "0":
+
+                lexema = lexema + self.leerCaracter()
+
+                # Segundo caracter es "otro"
+                # Return entero cero
+                if self.esDigito( lexema[1] ) == False:
+
+                    self.desleer()
+
+                    return {
+                            "token": pr.entero,
+                            "lexema": lexema[ 0 ]
+                        }
+
+                # Segundo caracter es un digito
+                else:
+
+                    # Se lee todo el numero entero para regresarlo como error y lexema
+                    digito = self.leerCaracter()
+
+                    while self.esDigito( digito ):
+                        lexema = lexema + digito
+                        digito = self.leerCaracter()
+                    
+                    return {
+                            "token": pr.error,
+                            "lexema": lexema
+                        }
+
+            # No empieza en cero
+            else:
+
+                # lee todo el numero y lo regresa como entero
+                digito = lexema
+                lexema = ""
+
+                while self.esDigito( digito ):
+                    lexema = lexema + digito
+                    digito = self.leerCaracter()
+                    
+                self.desleer()
+
+                return {
+                            "token": pr.entero,
+                            "lexema": lexema
+                        }
+
+        # Fin de Archivo
+        elif lexema is "\0":
+
+            return {
+                        "token": pr.hecho,
+                        "lexema": lexema
+                    }
+
+        # Delimitador vacio
+        elif lexema is " ":
+            
+            return{ 
+                "token": "vacio",
+                "lexema": lexema
+            }
+        
+        # Letra
+        elif self.esLetra(lexema):
+
+            # lee toda la palabra
+
+            caracter = lexema
+            lexema = ""
+
+            while self.esLetra( caracter ):
+                lexema = lexema + caracter
+                caracter = self.leerCaracter()
+                
+            self.desleer()
+
+            # La palabra leida es reservada
+            if lexema == "print":
+
+                return {
+                            "token": pr.imprimir,
+                            "lexema": lexema
+                        }
+
+            # La palabra NO es reservada
+            else:
+                return {
+                        "token": pr.letras,
+                        "lexema": lexema
+                    }
+
+        # Error  
+        else:
+            
+            return {
+                        "token": pr.error,
+                        "lexema": lexema
+                    }
